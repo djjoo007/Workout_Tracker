d9fbea2184e0eb31713b24ad25443bd5db308157
Joo Heredia <djjoo007uw@gmail.com>
2020-10-11 00:12:04 -0700

    add html-routes.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_UR || 'mongodb://localhost/workout', {
 });
 
 //Routes
-app.use(require('./routes/api.js'));
+app.use(require('./routes/api-routes.js'));
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}!`);
