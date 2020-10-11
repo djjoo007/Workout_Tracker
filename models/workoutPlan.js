cd3bff7ae4f5f0a4347167d3d350f932174c783c
Joo Heredia <djjoo007uw@gmail.com>
2020-10-11 00:38:31 -0700

    add new Schema

--- models/workoutPlan.js
+++ models/workoutPlan.js
@@ -3,8 +3,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercise: {
+        type: String,
+        trim: true,
+        required: 'Enter type of exercise.'
+    },
     name: {
-        
+        type: String,
+        trim: true,
+        required: 'Enter exercise name.'
+    },
+    duration: {
+        type: Number,
+        required: 'Enter exercise duration in minutes.'
+    },
+    weight: {
+        type: Number,
+        required: 'Enter total weight lifted.'
+    },
+    sets: {
+        type: Number,
+        required: 'Enter total sets performed.'
+    },
+    reps: {
+        type: Number,
+        required: 'Enter total reps performed.'
+    },
+    distance: {
+        type: Number,
+        required: 'Enter total distance covered.'
     }
 });
 
